a058c173421a752a30e3309aa663bae76e1c6f2c
Hexzhe <hexzhe@jsngbt.com>
2019-09-26 19:53:13 -0400

    Private/Publie reorder for consistency

--- 8PRO128-TP1/Point.h
+++ 8PRO128-TP1/Point.h
@@ -3,9 +3,6 @@
 
 class Point
 {
-private:
-	double X;
-	double Y;
 public:
 	Point(double x = 0, double y = 0);
 	~Point() {};
@@ -16,4 +13,7 @@ public:
 	Point MoveAbsolute(double x, double y);
 	Point MoveRelative(double countX, double countY);
 	std::string Info();
+private:
+	double X;
+	double Y;
 };
